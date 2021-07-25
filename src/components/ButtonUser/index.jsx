8d5d030913dd88a4e63960052bfae8bd7d2fa4ae
Ercüment Laçın <ercument.lacin@akinon.com>
2021-07-25 07:59:10 +0300

    current user photo added

--- src/components/ButtonUser/index.jsx
+++ src/components/ButtonUser/index.jsx
@@ -1,25 +1,42 @@
 import GetWidth from 'Hooks/GetWidth';
 import { RiMoreLine } from 'react-icons/ri';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { actionLogout } from 'store/actions/user';
+import { emailToUserName } from 'utils';
 import styles from './styles.module.scss';
+
+/** @typedef {Object} USER
+ * @property {String} displayName
+ * @property {String} email
+ * @property {String} photoURL
+ * @property {String} userId
+ */
 
 const ButtonUser = () => {
   const { width } = GetWidth();
-
+  /**
+   * @type {USER}
+   */
+  const currentUser = useSelector((state) => state.user.user);
   const dispatch = useDispatch();
   const onClickLogout = () => dispatch(actionLogout());
+
+  const userSlug = emailToUserName(currentUser.email);
 
   return (
     <div onClick={onClickLogout} className={styles.dFlex} aria-hidden="true">
       <div className={styles.img}>
-        <img src="assets/images/user_picture.jpg" alt="" />
+        <img
+          src={currentUser.photoURL}
+          alt={currentUser.displayName}
+          title="Çıkış yapmak için tıkla"
+        />
       </div>
       {width > 992 && (
         <>
           <div className={styles.text}>
-            <strong>Ercüment</strong>
-            <span>@ercument196</span>
+            <strong>{currentUser.displayName}</strong>
+            <span>@{userSlug}</span>
           </div>
           <div className={styles.more}>
             <RiMoreLine />
